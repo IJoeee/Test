250b380a38286e2770d84116b342d5f40883720b
IJoeee <128298350+IJoeee@users.noreply.github.com>
2024-07-01 01:13:53 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+from Functions.module1.module1 import print_from_module1
+from Functions.module2.module2 import print_from_module2
+
+def main():
+    print_from_module1()
+    print_from_module2()
+
+if __name__ == "__main__":
+    main()
