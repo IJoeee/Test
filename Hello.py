fec5b58aa6db262c46f0431e6cb9062f099ededd
IJoeee <128298350+IJoeee@users.noreply.github.com>
2025-02-18 01:30:29 -0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,2 +1,29 @@
-def hello_world():
-    print("Hello, World!")
+def greet():
+    print("Hello!")
+
+def farewell():
+    print("Goodbye!")
+
+def welcome():
+    print("Welcome!")
+
+def thank_you():
+    print("Thank you!")
+
+def sorry():
+    print("Sorry!")
+
+def laugh():
+    print("Haha!")
+
+def cheer():
+    print("Hooray!")
+
+def surprise():
+    print("Wow!")
+
+def agree():
+    print("I agree!")
+
+def deny():
+    print("I disagree!")
